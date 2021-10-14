ccd34be27c36026284ccaac1269e59064c124697
anubhav300 <86737429+anubhav300@users.noreply.github.com>
2021-10-14 09:52:43 +0530

    reverse_diamond

--- Beginner/pattern.py
+++ Beginner/pattern.py
@@ -7,8 +7,8 @@
 n = int(input()) #here n is the number of ars in one triangle
 
 
-gaps = n*2;
-layers = n+n-1;
+gaps = n*2
+layers = n+n-1
 for i in range(1,n+1):
     for j in range(i):
         print(8,end = "")
